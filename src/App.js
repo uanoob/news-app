e46d21b9f940e2b1db55d2eab44195f6170a700d
uanoob <usnet@ukr.net>
2018-11-14 13:09:45 +0200

    Connect store to app

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 
 import Select from 'react-select';
 import Articles from './components/Articles';
-import { getArticles } from './api';
 
 class App extends Component {
   state = {
-    articles: [],
     selection: null,
-  };
-
-  componentDidMount() {
-    this.getAllArticles();
-  }
-
-  getAllArticles = async () => {
-    const articles = await getArticles();
-    this.setState({ articles });
   };
 
   changeSelection = (selection) => {
@@ -27,22 +16,13 @@ class App extends Component {
   };
 
   render() {
-    const { articles, selection } = this.state;
-    const options = articles.map(article => ({
-      value: article._id,
-      label: article.title,
-    }));
-    return articles.length !== 0 ? (
+    const { selection } = this.state;
+    return (
       <div>
-        <Select
-          options={options}
-          value={selection}
-          onChange={this.changeSelection}
-          isMulti
-        />
-        <Articles articles={articles} />
+        <Select value={selection} onChange={this.changeSelection} isMulti />
+        <Articles />
       </div>
-    ) : null;
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import axios from 'axios';
+import { Provider } from 'react-redux';
+import store from './store/store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -10,7 +12,13 @@ import BASE_URL from './config';
 axios.defaults.baseURL = BASE_URL;
 axios.defaults.withCredentials = true;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
