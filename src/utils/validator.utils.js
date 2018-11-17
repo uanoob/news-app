f3f04eccdfc40eb8fe61884dd81615a2b96ee132
uanoob <usnet@ukr.net>
2018-11-17 16:48:44 +0200

    Add email validation

--- src/utils/validator.utils.js
+++ src/utils/validator.utils.js
@@ -1,8 +1,7 @@
-const checkValidity = (name, value) => {
+export const checkValidityLength = (name, value) => {
   const limits = {
     name: { min: 3, max: 20 },
-    password: { min: 6, max: 100 },
-    email: { min: 5, max: 50 },
+    password: { min: 3, max: 100 },
     text: { min: 20, max: 500 },
   };
   let isValid = true;
@@ -12,4 +11,8 @@ const checkValidity = (name, value) => {
   return isValid;
 };
 
-export default checkValidity;
+export const checkValidityEmail = (email) => {
+  // eslint-disable-next-line
+  const reg = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+  return reg.test(email.trim());
+};
