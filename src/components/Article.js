c89ed481f8156b7687723d8970a8f217ff2a3b6f
uanoob <usnet@ukr.net>
2018-11-18 14:09:47 +0200

    Add material-ua style -> Articles & Article

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,41 +1,124 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import Collapse from '@material-ui/core/Collapse';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import red from '@material-ui/core/colors/red';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Comments from './Comments';
 
+const styles = theme => ({
+  card: {
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+  actions: {
+    display: 'flex',
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+    marginLeft: 'auto',
+    [theme.breakpoints.up('sm')]: {
+      marginRight: -8,
+    },
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+});
+
 class Article extends Component {
-  static propTypes = {
-    article: PropTypes.shape({
-      _id: PropTypes.string.isRequired,
-      title: PropTypes.string.isRequired,
-      text: PropTypes.string.isRequired,
-      author_id: PropTypes.string.isRequired,
-      author_name: PropTypes.string.isRequired,
-      posted_at: PropTypes.string.isRequired,
-    }).isRequired,
-    isOpen: PropTypes.bool.isRequired,
-    toggleOpen: PropTypes.func.isRequired,
+  state = { expanded: false };
+
+  handleExpandClick = () => {
+    this.setState(state => ({ expanded: !state.expanded }));
   };
 
-  state = {};
-
   render() {
-    const { article, isOpen, toggleOpen } = this.props;
-    // console.log('---', 'render article');
+    const { classes, article } = this.props;
+    const { expanded } = this.state;
     return (
-      <div>
-        <h3>{article.title}</h3>
-        <button type="button" onClick={toggleOpen}>
-          {isOpen ? 'Close' : 'Open'}
-        </button>
-        {isOpen ? (
-          <section>
-            <p>{article.text}</p>
+      <Card className={classes.card}>
+        <CardHeader
+          avatar={(
+            <Avatar aria-label="Recipe" className={classes.avatar}>
+              R
+            </Avatar>
+)}
+          action={(
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+)}
+          title={article.title}
+          subheader={article.posted_at}
+        />
+
+        <CardContent>
+          <Typography component="p">{article.text}</Typography>
+        </CardContent>
+
+        <CardActions className={classes.actions} disableActionSpacing>
+          <IconButton aria-label="Add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="Share">
+            <ShareIcon />
+          </IconButton>
+          <IconButton
+            className={classnames(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+            onClick={this.handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="Show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
             <Comments articleId={article._id} />
-          </section>
-        ) : null}
-      </div>
+          </CardContent>
+        </Collapse>
+      </Card>
     );
   }
 }
 
-export default Article;
+Article.propTypes = {
+  classes: PropTypes.shape({
+    card: PropTypes.string.isRequired,
+    actions: PropTypes.string.isRequired,
+    expand: PropTypes.string.isRequired,
+    expandOpen: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+  }).isRequired,
+  article: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    author_id: PropTypes.string.isRequired,
+    author_name: PropTypes.string.isRequired,
+    posted_at: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default withStyles(styles)(Article);
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,34 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
 import Article from './Article';
-import accordion from '../decorators/accordion';
 import { getAllArticles } from '../store/actions';
-import articlesSelector from '../selectors/article.selector';
+
+const styles = theme => ({
+  root: {
+    margin: 'auto',
+    marginTop: 20,
+    width: '100%',
+    maxWidth: 600,
+    backgroundColor: theme.palette.background.paper,
+  },
+  nested: {
+    paddingLeft: theme.spacing.unit * 4,
+  },
+});
 
 class Articles extends Component {
   state = {};
-
-  static defaultProps = {
-    articles: [],
-    openItemId: null,
-  };
-
-  static propTypes = {
-    articles: PropTypes.arrayOf(
-      PropTypes.shape({
-        _id: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        text: PropTypes.string.isRequired,
-        author_id: PropTypes.string.isRequired,
-        author_name: PropTypes.string.isRequired,
-        posted_at: PropTypes.string.isRequired,
-      }).isRequired,
-    ),
-    onGetAllArticles: PropTypes.func.isRequired,
-    toggleOpenItem: PropTypes.func.isRequired,
-    openItemId: PropTypes.string,
-  };
 
   componentDidMount() {
     const { onGetAllArticles } = this.props;
@@ -36,26 +29,45 @@ class Articles extends Component {
   }
 
   render() {
-    const { articles, openItemId, toggleOpenItem } = this.props;
-    // console.log('---', 'render article list');
+    const { classes, articles } = this.props;
     return articles && articles.length !== 0 ? (
-      <ul>
-        {articles.map(article => (
-          <li key={article._id}>
-            <Article
-              article={article}
-              isOpen={article._id === openItemId}
-              toggleOpen={() => toggleOpenItem(article._id)}
-            />
-          </li>
-        ))}
-      </ul>
+      <div className={classes.root}>
+        <List component="nav">
+          {articles.map(article => (
+            <ListItem key={article._id}>
+              <Article article={article} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
     ) : null;
   }
 }
 
+Articles.defaultProps = {
+  articles: [],
+};
+
+Articles.propTypes = {
+  classes: PropTypes.shape({
+    root: PropTypes.string.isRequired,
+    nested: PropTypes.string.isRequired,
+  }).isRequired,
+  articles: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+      author_id: PropTypes.string.isRequired,
+      author_name: PropTypes.string.isRequired,
+      posted_at: PropTypes.string.isRequired,
+    }).isRequired,
+  ),
+  onGetAllArticles: PropTypes.func.isRequired,
+};
+
 const mapStateToProps = state => ({
-  articles: articlesSelector(state),
+  articles: state.articles.articles,
 });
 
 const mapDispatchToProps = {
@@ -65,4 +77,4 @@ const mapDispatchToProps = {
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
-)(accordion(Articles));
+)(withStyles(styles)(Articles));
