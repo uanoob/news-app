a315fa1cce2aae8d3f19dab6bfe93b29c829a193
uanoob <usnet@ukr.net>
2018-11-20 17:20:20 +0200

    Create utils -> avatar & stringToColor

---
+++ src/utils/avatar.utils.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line
+const handleAuthorAvatar = str => `${str.charAt(0)}${str.charAt(str.length - 1)}`.toUpperCase();
+
+export default handleAuthorAvatar;
---
+++ src/utils/stringToColor.utils.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+const stringToColour = function(str) {
+  let hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  let colour = '#';
+  for (var i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xff;
+    colour += `00${value.toString(16)}`.substr(-2);
+  }
+  return colour;
+};
+
+export default stringToColour;
+/* eslint-enable */
