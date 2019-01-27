279ab6c423ac89955010b64681b797a5c8017f61
uanoob <usnet@ukr.net>
2019-01-27 15:08:51 +0200

    Add tests to CommentForm component

--- src/components/comment/__tests__/CommentForm.spec.js
+++ src/components/comment/__tests__/CommentForm.spec.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { CommentFormComponent } from '../CommentForm';
+import configureMockStore from 'redux-mock-store';
+import CommentForm, { CommentFormComponent } from '../CommentForm';
 
 describe('CommentForm', () => {
   const mockCreateComment = jest.fn();
   const mockUpdateComment = jest.fn();
   const mockHandleDialogClick = jest.fn();
-  const mockClearTextField = jest.fn();
   const initialState = {
     comment: '',
     commentInputTouched: false,
@@ -24,12 +24,12 @@ describe('CommentForm', () => {
     userName: 'PropTypes.string.isRequired',
     dialog: false,
     handleDialogClick: mockHandleDialogClick,
-    clearTextField: mockClearTextField,
     title: 'PropTypes.string.isRequired',
     description: 'PropTypes.string.isRequired',
     commentId: '',
     commentText: 'PropTypes.string',
   };
+
   describe('CommentFormComponent initial', () => {
     const wrapper = shallow(<CommentFormComponent {...props} />);
     it('render correctly CommentFormComponent', () => {
@@ -86,11 +86,19 @@ describe('CommentForm', () => {
   });
   describe('CommentFormComponent submit', () => {
     const wrapper = shallow(<CommentFormComponent {...props} />);
+    const instance = wrapper.instance();
+    jest.spyOn(instance, 'clearTextField');
     describe('when clicking the Send button and no commentId', () => {
       const event = { preventDefault: () => {} };
       wrapper.find('#comment-submit-button').simulate('click', event);
       it('calls the onCreateComment', () => {
         expect(mockCreateComment).toBeCalledTimes(1);
+      });
+      it('calls clearTextField', () => {
+        expect(instance.clearTextField).toBeCalledTimes(1);
+      });
+      it('set state to initialState', () => {
+        expect(wrapper.state()).toEqual(initialState);
       });
     });
   });
@@ -100,12 +108,70 @@ describe('CommentForm', () => {
       commentId: '123',
     };
     const wrapper = shallow(<CommentFormComponent {...nextProps} />);
+    const instance = wrapper.instance();
+    jest.spyOn(instance, 'clearTextField');
     describe('when clicking the Send button and commentId', () => {
       const event = { preventDefault: () => {} };
       wrapper.find('#comment-submit-button').simulate('click', event);
       it('calls onUpdateComment', () => {
         expect(mockUpdateComment).toBeCalledTimes(1);
       });
+      it('calls clearTextField', () => {
+        expect(instance.clearTextField).toBeCalledTimes(1);
+      });
+      it('set state to initialState', () => {
+        expect(wrapper.state()).toEqual(initialState);
+      });
+    });
+  });
+  describe('CommentFormComponent cancel', () => {
+    const wrapper = shallow(<CommentFormComponent {...props} />);
+    const instance = wrapper.instance();
+    jest.spyOn(instance, 'clearTextField');
+    describe('when clicking the Cancel button', () => {
+      wrapper.find('#comment-cancel-button').simulate('click');
+      it('calls clearTextField', () => {
+        expect(instance.clearTextField).toBeCalledTimes(1);
+      });
+    });
+  });
+  describe('CommentForm mapStateToProps & mapDispatchToProps', () => {
+    const mockStore = configureMockStore();
+    const createComment = () => ({
+      type: 'CREATE_COMMENT',
+    });
+    const updateComment = () => ({
+      type: 'UPDATE_COMMENT',
+    });
+    const user = {
+      user: {
+        _id: 'testid',
+        name: 'testname',
+      },
+    };
+    const initialStateRedux = {
+      user,
+    };
+    let store;
+    let wrapper;
+    beforeEach(() => {
+      store = mockStore(initialStateRedux);
+      wrapper = shallow(<CommentForm store={store} />);
+    });
+    it('the state values were correctly passed as props', () => {
+      expect(wrapper.props().store.getState()).toEqual(initialStateRedux);
+    });
+    it('the store dispatched the action CREATE_COMMENT', () => {
+      store.dispatch(createComment());
+      const actions = store.getActions();
+      const expectedPayload = { type: 'CREATE_COMMENT' };
+      expect(actions).toEqual([expectedPayload]);
+    });
+    it('the store dispatched the action UPDATE_COMMENT', () => {
+      store.dispatch(updateComment());
+      const actions = store.getActions();
+      const expectedPayload = { type: 'UPDATE_COMMENT' };
+      expect(actions).toEqual([expectedPayload]);
     });
   });
 });
